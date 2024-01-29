52976e982132e190d202d844e31c7344a89e4486
aaaasdfghjkllll <157814835+aaaasdfghjkllll@users.noreply.github.com>
2024-01-29 10:11:09 +0100

    Update OPplayerTPmodule to look through player list instead of players in visual range

--- src/main/java/pwn/noobs/trouserstreak/modules/OPplayerTPmodule.java
+++ src/main/java/pwn/noobs/trouserstreak/modules/OPplayerTPmodule.java
@@ -1,5 +1,6 @@
 package pwn.noobs.trouserstreak.modules;
 
+import meteordevelopment.meteorclient.events.game.GameJoinedEvent;
 import meteordevelopment.meteorclient.gui.GuiTheme;
 import meteordevelopment.meteorclient.gui.widgets.WWidget;
 import meteordevelopment.meteorclient.gui.widgets.containers.WTable;
@@ -9,10 +10,14 @@ import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.player.ChatUtils;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.client.network.PlayerListEntry;
 import pwn.noobs.trouserstreak.Trouser;
-import java.util.List;
-//credits to ogmur (https://www.youtube.com/@Ogmur) for the idea and etianl for writing
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+//credits to ogmur (https://www.youtube.com/@Ogmur) for the idea, etianl for writing and aaaasdfghjkllll for fixing
 public class OPplayerTPmodule extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
     public final Setting<Boolean> tp2u = sgGeneral.add(new BoolSetting.Builder()
@@ -30,42 +35,46 @@ public class OPplayerTPmodule extends Module {
     public WWidget getWidget(GuiTheme theme) {
         WTable table = theme.table();
         WButton deletedata = table.add(theme.button("RESET CURRENT PLAYER")).expandX().minWidth(100).widget();
-        deletedata.action = () -> {
-            currentplayer = 0;
-        };
+        deletedata.action = () -> i = 0;
         table.row();
         return table;
     }
-    public static int currentplayer = 0;
     public OPplayerTPmodule() {
         super(Trouser.Main, "OPplayerTPmodule", "**REQUIRES OP** Teleports you to each player on the server with a button press if keybound, or teleport people to you.");
     }
-    private List<AbstractClientPlayerEntity> players = null;
+
+    private int i = 0;
 
     @Override
     public void onActivate() {
         if (notOP.get() && !(mc.player.hasPermissionLevel(2)) && mc.world.isChunkLoaded(mc.player.getChunkPos().x, mc.player.getChunkPos().z)) {
             toggle();
             error("Must have permission level 2 or higher");
-        }
-        players = mc.world.getPlayers();
-        if (currentplayer<players.size()) currentplayer++;
-        if (players.get(currentplayer-1) == mc.player) {
-            //skip past yourself
-            currentplayer++;
-        }
-        if (players.size()==1){
-            error("No other players online.");
-            currentplayer=0;
-            toggle();
             return;
         }
-        if (currentplayer>=players.size()) currentplayer=players.size();
-        error("Player "+currentplayer+" of "+players.size());
-        if (tp2u.get()) ChatUtils.sendPlayerMsg("/tp "+mc.player.getName().getLiteralString()+" "+players.get(currentplayer-1).getName().getLiteralString());
-        else if (!tp2u.get()) ChatUtils.sendPlayerMsg("/tp "+players.get(currentplayer-1).getName().getLiteralString()+" "+mc.player.getName().getLiteralString());
-        error(String.valueOf(players.get(currentplayer-1).getName().getLiteralString()));
-        if (currentplayer>=players.size()) currentplayer=0;
+        Collection<PlayerListEntry> playerListEntries = mc.getNetworkHandler().getPlayerList();
+        ArrayList<PlayerListEntry> playerArrayList = new ArrayList<>(playerListEntries);
+        if(playerArrayList.get(i).getProfile().getName().equals(mc.player.getName().getLiteralString())) i++;
+        if(i-1 >= playerArrayList.size()) i = 0;
+        if(playerArrayList.size() == 1) {
+            error("No other players online.");
+            toggle();
+            i = 0;
+            return;
+        }
+        if(tp2u.get()) ChatUtils.sendPlayerMsg("/tp " + playerArrayList.get(i).getProfile().getName() + " " + mc.player.getName().getLiteralString());
+        if (!tp2u.get()) ChatUtils.sendPlayerMsg("/tp " + mc.player.getName().getLiteralString() + " " + playerArrayList.get(i).getProfile().getName());
+        i++;
         toggle();
+    }
+
+    @EventHandler
+    private void onGameJoin(GameJoinedEvent event) {
+        i = 0;
+    }
+
+    @Override
+    public void onDeactivate() {
+        i = 0;
     }
 }
