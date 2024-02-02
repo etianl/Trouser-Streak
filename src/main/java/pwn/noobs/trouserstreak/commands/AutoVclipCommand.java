2daa43b002a9860033d011da970079be95eedd8a
agreed31579 <117580989+agreed31579@users.noreply.github.com>
2024-02-02 13:33:02 +0200

    Update AutoVclipCommand.java

--- src/main/java/pwn/noobs/trouserstreak/commands/AutoVclipCommand.java
+++ src/main/java/pwn/noobs/trouserstreak/commands/AutoVclipCommand.java
@@ -1,6 +1,5 @@
 package pwn.noobs.trouserstreak.commands;
 
-import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import meteordevelopment.meteorclient.commands.Command;
 import net.minecraft.block.Blocks;
@@ -8,8 +7,6 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.command.CommandSource;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import net.minecraft.network.packet.c2s.play.VehicleMoveC2SPacket;
 
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 import static meteordevelopment.meteorclient.MeteorClient.mc;
@@ -21,27 +18,20 @@ public class AutoVclipCommand extends Command {
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.executes(ctx -> {
-            error("Choose Up, Down or Highest");
+            error("Choose Up or Down.");
             return SINGLE_SUCCESS;
         });
         builder.then(literal("up").executes(ctx -> {
             ClientPlayerEntity player = mc.player;
             assert player != null;
-            for (int i = 0; i < 199; i++) {
+            for (int i = 0; i < 9; i++) {
                 BlockPos isopenair1 = (player.getBlockPos().add(0,i+2,0));
                 BlockPos isopenair2 = (player.getBlockPos().add(0,i+3,0));
                 if (mc.world.getBlockState(isopenair1).isReplaceable() && mc.world.getFluidState(isopenair1).isEmpty() && !mc.world.getBlockState(isopenair1).isOf(Blocks.POWDER_SNOW) && mc.world.getBlockState(isopenair2).isReplaceable() && mc.world.getFluidState(isopenair2).isEmpty() && !mc.world.getBlockState(isopenair2).isOf(Blocks.POWDER_SNOW)){
-					int packetsRequired = 20;
                     if (player.hasVehicle()) {
                         Entity vehicle = player.getVehicle();
-						for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
-							mc.player.networkHandler.sendPacket(new VehicleMoveC2SPacket(mc.player.getVehicle()));
-						}
                         vehicle.setPosition(vehicle.getX(), isopenair1.getY(), vehicle.getZ());
                     }
-					for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
-						mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true));
-					}
                     player.setPosition(player.getX(), isopenair1.getY(), player.getZ());
                     return SINGLE_SUCCESS;
                 }
@@ -52,21 +42,14 @@ public class AutoVclipCommand extends Command {
         builder.then(literal("down").executes(ctx -> {
             ClientPlayerEntity player = mc.player;
             assert player != null;
-            for (int i = 0; i > -199; i--) {
+            for (int i = 0; i > -10; i--) {
                 BlockPos isopenair1 = (player.getBlockPos().add(0,i,0));
                 BlockPos isopenair2 = (player.getBlockPos().add(0,i-1,0));
                 if (mc.world.getBlockState(isopenair1).isReplaceable() && mc.world.getFluidState(isopenair1).isEmpty() && !mc.world.getBlockState(isopenair1).isOf(Blocks.POWDER_SNOW) && mc.world.getBlockState(isopenair2).isReplaceable() && mc.world.getFluidState(isopenair2).isEmpty() && !mc.world.getBlockState(isopenair2).isOf(Blocks.POWDER_SNOW)){
-                    int packetsRequired = 20;
-					if (player.hasVehicle()) {
+                    if (player.hasVehicle()) {
                         Entity vehicle = player.getVehicle();
-						for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
-							mc.player.networkHandler.sendPacket(new VehicleMoveC2SPacket(mc.player.getVehicle()));
-						}
                         vehicle.setPosition(vehicle.getX(), isopenair2.getY(), vehicle.getZ());
                     }
-					for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
-						mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true));
-					}
                     player.setPosition(player.getX(), isopenair2.getY(), player.getZ());
                     return SINGLE_SUCCESS;
                 }
@@ -74,33 +57,5 @@ public class AutoVclipCommand extends Command {
             error("No gap found to vclip into");
             return SINGLE_SUCCESS;
         }));
-		builder.then(literal("highest").executes(ctx -> {
-			ClientPlayerEntity player = mc.player;
-			assert player != null;
-
-			for (int i = 199; i > 0; i--) {
-				BlockPos isopenair1 = (player.getBlockPos().add(0,i,0));
-				BlockPos isopenair2 = (player.getBlockPos().add(0,i-1,0));
-				BlockPos newopenair2 = isopenair2.up(2);
-				if (!mc.world.getBlockState(isopenair1).isReplaceable() && !mc.world.getBlockState(isopenair1).isOf(Blocks.POWDER_SNOW) && mc.world.getFluidState(isopenair1).isEmpty() && !mc.world.getBlockState(isopenair2).isReplaceable() && !mc.world.getBlockState(isopenair2).isOf(Blocks.POWDER_SNOW) && mc.world.getFluidState(isopenair2).isEmpty()) {
-					int packetsRequired = 20;
-					if (player.hasVehicle()) {
-                        Entity vehicle = player.getVehicle();
-						for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
-							mc.player.networkHandler.sendPacket(new VehicleMoveC2SPacket(mc.player.getVehicle()));
-						}
-
-                        vehicle.setPosition(vehicle.getX(), newopenair2.getY(), vehicle.getZ());
-                    }
-					for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
-						mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true));
-					}
-                    player.setPosition(player.getX(), newopenair2.getY(), player.getZ());
-                    return SINGLE_SUCCESS;
-                }
-            }
-            error("No gap found to vclip into");
-            return SINGLE_SUCCESS;
-		}));
-	}
+    }
 }
