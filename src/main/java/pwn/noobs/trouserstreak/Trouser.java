ce5f168e3e610e22db3a666a969ef75d49a7094c
etianl <115842502+etianl@users.noreply.github.com>
2024-06-17 13:45:26 -0700

    Make Trouser.java look better on anticope

--- src/main/java/pwn/noobs/trouserstreak/Trouser.java
+++ src/main/java/pwn/noobs/trouserstreak/Trouser.java
@@ -13,56 +13,67 @@ import pwn.noobs.trouserstreak.modules.*;
 public class Trouser extends MeteorAddon {
         public static final Logger LOG = LoggerFactory.getLogger(Trouser.class);
         public static final Category Main = new Category("TrouserStreak");
-
+        //false commented out modules to categorize modules when displayed on the anticope page
         @Override
         public void onInitialize() {
                 LOG.info("Initializing PantsMod!");
-                Modules.get().add(new AutoLavaCaster());
+                //Modules.get().add(new -----> Automated Lavacasting Features! Make Lava Mountains fast and easily! <-----());
                 Modules.get().add(new AutoMountain());
+                Modules.get().add(new AutoLavaCaster());
                 Modules.get().add(new AutoStaircase());
-                Modules.get().add(new TrouserBuild());
-                Modules.get().add(new TrailMaker());
+                Commands.add(new LavaTimeCalculator());
+                Commands.add(new CasterTimer());
+
+                //Modules.get().add(new -----> Find and Grief noobs! <-----());
                 Modules.get().add(new NewerNewChunks());
+                Modules.get().add(new BaseFinder());
                 Modules.get().add(new StorageLooter());
+                Modules.get().add(new LavaAura());
+                Modules.get().add(new InstantKill());
                 Modules.get().add(new SuperInstaMine());
                 Modules.get().add(new InstaMineNuker());
-                Modules.get().add(new InstaSafetyBox());
-                Modules.get().add(new BaseFinder());
-                Modules.get().add(new Teleport());
-                Modules.get().add(new TPFly());
-                Modules.get().add(new HandOfGod());
-                Modules.get().add(new OPServerKillModule());
-                Modules.get().add(new OPplayerTPmodule());
-                Modules.get().add(new ExplosionAura());
-                Modules.get().add(new ShulkerDupe());
-                Modules.get().add(new InvDupeModule());
-                Modules.get().add(new InstantKill());
-                Modules.get().add(new LavaAura());
-                Modules.get().add(new LecternCrash());
-                Modules.get().add(new AutoDrop());
-                Modules.get().add(new NbtEditor());
-                Modules.get().add(new AnHero());
-                Modules.get().add(new RedstoneNuker());
-                Modules.get().add(new AirstrikePlus());
-                Modules.get().add(new BoomPlus());
-                Modules.get().add(new VoiderPlus());
                 Modules.get().add(new BetterScaffold());
-                Modules.get().add(new BetterAutoSign());
-                Modules.get().add(new FlightAntikick());
-                Modules.get().add(new BlockListMineCommand());
+                Modules.get().add(new RedstoneNuker());
+
+                //Modules.get().add(new -----> Overpowered OP mode modules! <-----());
+                Modules.get().add(new HandOfGod());
+                Modules.get().add(new VoiderPlus());
+                Modules.get().add(new OPServerKillModule());
+                Commands.add(new CrashCommand());
                 Modules.get().add(new AutoCommand());
                 Modules.get().add(new AutoScoreboard());
                 Modules.get().add(new AutoTitles());
                 Modules.get().add(new AutoDisplays());
-                Commands.add(new LavaTimeCalculator());
-                Commands.add(new CasterTimer());
-                Commands.add(new NewChunkCounter());
-                Commands.add(new BaseFinderCommands());
+                Modules.get().add(new OPplayerTPmodule());
+
+                //Modules.get().add(new -----> Create Illegal things with Creative mode! <-----());
+                Modules.get().add(new NbtEditor());
+                Modules.get().add(new AirstrikePlus());
+                Modules.get().add(new BoomPlus());
+                Modules.get().add(new ExplosionAura());
+
+                //Modules.get().add(new -----> Exploits for old versions! <-----());
+                Modules.get().add(new ShulkerDupe());
+                Modules.get().add(new InvDupeModule());
+                Modules.get().add(new LecternCrash());
+
+                //Modules.get().add(new -----> And much more <-----());
+                Modules.get().add(new BetterAutoSign());
+                Modules.get().add(new Teleport());
+                Modules.get().add(new TPFly());
+                Modules.get().add(new FlightAntikick());
+                Modules.get().add(new InstaSafetyBox());
+                Modules.get().add(new TrouserBuild());
+                Modules.get().add(new TrailMaker());
+                Modules.get().add(new AutoDrop());
+                Modules.get().add(new AnHero());
                 Commands.add(new WorldInfoCommand());
                 Commands.add(new ViewNbtCommand());
                 Commands.add(new AutoVclipCommand());
                 Commands.add(new AutoVaultClipCommand());
-                Commands.add(new CrashCommand());
+                Modules.get().add(new BlockListMineCommand());
+                Commands.add(new NewChunkCounter());
+                Commands.add(new BaseFinderCommands());
                 Commands.add(new GarbageCleanerCommand());
         }
 
