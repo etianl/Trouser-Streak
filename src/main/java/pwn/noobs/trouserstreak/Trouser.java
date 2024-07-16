f5f0dbf8853d92238cd269a858be5fc27f4166f9
etianl <115842502+etianl@users.noreply.github.com>
2024-07-16 10:51:30 -0700

    Update Trouser.java for Anticope webpage

--- src/main/java/pwn/noobs/trouserstreak/Trouser.java
+++ src/main/java/pwn/noobs/trouserstreak/Trouser.java
@@ -17,7 +17,7 @@ public class Trouser extends MeteorAddon {
         @Override
         public void onInitialize() {
                 LOG.info("Initializing PantsMod!");
-                //Modules.get().add(new !!!!!Featuring powerful Griefing Utilities and Home of the Original PaletteExploit for NewChunks!!!!!());
+                //Modules.get().add(new !!!!!Featuring powerful Griefing Tools and Home of the Original PaletteExploit for NewChunks!!!!!());
                 //Modules.get().add(new -----> Automated Lavacasting Features! Make Lava Mountains fast and easily! <-----());
                 Modules.get().add(new AutoMountain());
                 Modules.get().add(new AutoLavaCaster());
