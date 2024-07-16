60f606202bea108c5ffc08bda5e931a07aabfc91
etianl <115842502+etianl@users.noreply.github.com>
2024-07-16 10:49:53 -0700

    Update Trouser.java for anticope website
    make it give better credit for newernewchunks

--- src/main/java/pwn/noobs/trouserstreak/Trouser.java
+++ src/main/java/pwn/noobs/trouserstreak/Trouser.java
@@ -17,8 +17,7 @@ public class Trouser extends MeteorAddon {
         @Override
         public void onInitialize() {
                 LOG.info("Initializing PantsMod!");
-                //Modules.get().add(new !!!!!Featuring powerful Griefing Utilities and NewChunks Exploits!!!!!());
-                //Modules.get().add(new );
+                //Modules.get().add(new !!!!!Featuring powerful Griefing Utilities and Home of the Original PaletteExploit for NewChunks!!!!!());
                 //Modules.get().add(new -----> Automated Lavacasting Features! Make Lava Mountains fast and easily! <-----());
                 Modules.get().add(new AutoMountain());
                 Modules.get().add(new AutoLavaCaster());
