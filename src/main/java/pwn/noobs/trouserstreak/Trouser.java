adbad7bb03be7eb7e64b365417f36360c726aeb8
etianl <115842502+etianl@users.noreply.github.com>
2024-08-14 01:45:54 -0700

    *1.2.1* Add Stairs detection to Hole/TunnelESP, bugfixes
    ***Made Hole/TunnelFinder Great Again***
    - Renamed module to **Hole/Tunnel/Stairs Finder**.
    - Added an option for detecting staircases tunnels that people have dug.
    - Fixed **Hole/Tunnel/Stairs Finder** not finding holes or tunnels below Y0. I'm pretty sure I caused this bug in a previous update.
    - Bumped up default amount of chunks to process per tick from 10 to 20.
    - Attempted to improve code a bit in that module?

--- src/main/java/pwn/noobs/trouserstreak/Trouser.java
+++ src/main/java/pwn/noobs/trouserstreak/Trouser.java
@@ -29,7 +29,7 @@ public class Trouser extends MeteorAddon {
                 //Modules.get().add(new -----> Find and Grief noobs! <-----());
                 Modules.get().add(new NewerNewChunks());
                 Modules.get().add(new BaseFinder());
-                Modules.get().add(new HoleAndTunnelFinder());
+                Modules.get().add(new HoleAndTunnelAndStairs_Finder());
                 Modules.get().add(new TrouserBlockESP());
                 Modules.get().add(new StorageLooter());
                 Modules.get().add(new LavaAura());
