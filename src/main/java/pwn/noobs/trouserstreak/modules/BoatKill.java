0cfd55c3e227b8c159abbee05addc3bc1613b5b8
nxyi <ARealEmail@gmail.com>
2024-09-15 21:22:04 +0100

    Improve boatInstaKill

--- src/main/java/pwn/noobs/trouserstreak/modules/BoatKill.java
+++ src/main/java/pwn/noobs/trouserstreak/modules/BoatKill.java
@@ -5,7 +5,10 @@ import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.player.ChatUtils;
+import net.fabricmc.loader.impl.lib.sat4j.core.Vec;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.network.packet.c2s.play.PlayerInputC2SPacket;
 import net.minecraft.network.packet.c2s.play.VehicleMoveC2SPacket;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.Vec3d;
@@ -16,31 +19,21 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class BoatKill extends Module {
+    private final SettingGroup settingGroup = settings.getDefaultGroup();
+
+    // all heights should work fine. and since we are in a boat. tp limit rises to 400 instead of 200.
+    private final Setting<Integer> height = settingGroup.add(new IntSetting.Builder()
+            .name("Height")
+            .description("Height to use for boatKill")
+            .defaultValue(111)
+            .min(1)
+            .sliderRange(1,200)
+            .build()
+    );
 
     public BoatKill() {
         super(Trouser.Main, "BoatKill", "Kill everyone in a boat using funny packets.");
     }
-
-    private final SettingGroup repeatGroup = settings.createGroup("Repeat");
-    private final SettingGroup delayGroup = settings.createGroup("Delay");
-    private final Setting<Integer> repeat = repeatGroup.add(new IntSetting.Builder()
-            .name("Repeat")
-            .description("Number of times to repeat the action")
-            .defaultValue(20)
-            .min(1)
-            .sliderRange(1,100)
-            .build()
-    );
-    private final Setting<Integer> delay = delayGroup.add(new IntSetting.Builder()
-            .name("Delay")
-            .description("Delay between each action in seconds")
-            .defaultValue(0)
-            .min(0)
-            .sliderRange(0,100)
-            .build()
-    );
-
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     @Override
     public void onActivate() {
@@ -49,18 +42,26 @@ public class BoatKill extends Module {
             toggle();
             return;
         }
-        Vec3d originalPos = boat.getPos();
-        boat.setPosition(originalPos.add(0, 0.05, 0));
-        VehicleMoveC2SPacket groundPacket = new VehicleMoveC2SPacket(boat);
-        boat.setPosition(originalPos.add(0, 20, 0));
-        VehicleMoveC2SPacket skyPacket = new VehicleMoveC2SPacket(boat);
-        boat.setPosition(originalPos);
-        for (int i = 0; i < repeat.get(); i++) { // 디폴트값:20
-            scheduler.schedule(() -> mc.player.networkHandler.sendPacket(skyPacket), delay.get(), TimeUnit.SECONDS);
-            scheduler.schedule(() -> mc.player.networkHandler.sendPacket(groundPacket), delay.get(), TimeUnit.SECONDS);
+
+        Vec3d oPos = boat.getPos();
+
+        // cba calculating the actual packets for this. 15 should be more than enough
+        for (int i = 0; i < 15; i++) {
+            moveTo(oPos);
         }
-        mc.player.networkHandler.sendPacket(new VehicleMoveC2SPacket(boat));
-        ChatUtils.sendMsg(Text.of("exploit executed successfully."));
+
+        moveTo(oPos.add(0,height.get(),0));
+
+        // floating point is what makes the boat break.
+        moveTo(oPos.add(0,0.0001,0));
+
+        mc.player.networkHandler.sendPacket(new PlayerInputC2SPacket(0,0,false,true));
+        mc.player.dismountVehicle();
         toggle();
+    }
+
+    public void moveTo(Vec3d pos){
+        mc.player.getVehicle().setPosition(pos);
+        mc.player.networkHandler.sendPacket(new VehicleMoveC2SPacket(mc.player.getVehicle()));
     }
 }
