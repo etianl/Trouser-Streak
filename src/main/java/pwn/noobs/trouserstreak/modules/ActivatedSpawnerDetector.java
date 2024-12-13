dcdf86e6508532473f19dc7d07be8abcb447f0d0
etianl <115842502+etianl@users.noreply.github.com>
2024-12-13 15:14:28 -0800

    v1.3.1 Deactivated spawner detection 4 ActivatedSpawnerDetecter
    **Deactivated Spawner Detector for the ActivatedSpawnederDetector**
    - Added a detector for this which flags spawners that have light blocks within a certain radius of them

--- src/main/java/pwn/noobs/trouserstreak/modules/ActivatedSpawnerDetector.java
+++ src/main/java/pwn/noobs/trouserstreak/modules/ActivatedSpawnerDetector.java
@@ -10,6 +10,7 @@ import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import meteordevelopment.meteorclient.utils.render.color.Color;
 import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.MobSpawnerBlockEntity;
 import net.minecraft.text.Text;
@@ -39,12 +40,33 @@ public class ActivatedSpawnerDetector extends Module {
             .defaultValue(true)
             .build()
     );
+    private final Setting<Boolean> deactivatedSpawner = sgGeneral.add(new BoolSetting.Builder()
+            .name("De-Activated Spawner Detector")
+            .description("Detects spawners with torches on them.")
+            .defaultValue(true)
+            .build()
+    );
+    public final Setting<Integer> deactivatedSpawnerdistance = sgGeneral.add(new IntSetting.Builder()
+            .name("Torch Scan distance")
+            .description("How many blocks from the spawner to look for blocks that make light")
+            .defaultValue(1)
+            .min(1)
+            .sliderRange(1,10)
+            .visible(() -> deactivatedSpawner.get())
+            .build()
+    );
     public final Setting<Integer> renderDistance = sgRender.add(new IntSetting.Builder()
             .name("Render-Distance(Chunks)")
             .description("How many chunks from the character to render the detected chunks.")
             .defaultValue(32)
             .min(6)
             .sliderRange(6,1024)
+            .build()
+    );
+    private final Setting<Boolean> removerenderdist = sgRender.add(new BoolSetting.Builder()
+            .name("RemoveOutsideRenderDistance")
+            .description("Removes the cached chunks when they leave the defined render distance.")
+            .defaultValue(true)
             .build()
     );
     private final Setting<ShapeMode> shapeMode = sgRender.add(new EnumSetting.Builder<ShapeMode>()
@@ -67,6 +89,20 @@ public class ActivatedSpawnerDetector extends Module {
             .visible(() -> (shapeMode.get() == ShapeMode.Lines || shapeMode.get() == ShapeMode.Both))
             .build()
     );
+    private final Setting<SettingColor> despawnerSideColor = sgRender.add(new ColorSetting.Builder()
+            .name("deactivated-spawner-side-color")
+            .description("Color of the spawner with torches.")
+            .defaultValue(new SettingColor(251, 5, 251, 70))
+            .visible(() -> deactivatedSpawner.get() && (shapeMode.get() == ShapeMode.Sides || shapeMode.get() == ShapeMode.Both))
+            .build()
+    );
+    private final Setting<SettingColor> despawnerLineColor = sgRender.add(new ColorSetting.Builder()
+            .name("deactivated-spawner-line-color")
+            .description("Color of the spawner with torches.")
+            .defaultValue(new SettingColor(251, 5, 251, 235))
+            .visible(() -> deactivatedSpawner.get() && (shapeMode.get() == ShapeMode.Lines || shapeMode.get() == ShapeMode.Both))
+            .build()
+    );
     private final Setting<Boolean> rangerendering = sgRender.add(new BoolSetting.Builder()
             .name("spawner-range-rendering")
             .description("Renders the rough active range of a mob spawner block.")
@@ -87,14 +123,9 @@ public class ActivatedSpawnerDetector extends Module {
             .visible(() -> rangerendering.get() && (shapeMode.get() == ShapeMode.Lines || shapeMode.get() == ShapeMode.Both))
             .build()
     );
-    private final Setting<Boolean> removerenderdist = sgRender.add(new BoolSetting.Builder()
-            .name("RemoveOutsideRenderDistance")
-            .description("Removes the cached chunks when they leave the defined render distance.")
-            .defaultValue(true)
-            .build()
-    );
 
     private final Set<BlockPos> spawnerPositions = Collections.synchronizedSet(new HashSet<>());
+    private final Set<BlockPos> deactivatedSpawnerPositions = Collections.synchronizedSet(new HashSet<>());
 
     public ActivatedSpawnerDetector() {
         super(Trouser.Main,"ActivatedSpawnerDetector", "Detects if a player has been near a mob spawner in the past. May be useful for finding player made stashes in dungeons, mineshafts, and other places.");
@@ -102,10 +133,12 @@ public class ActivatedSpawnerDetector extends Module {
     @Override
     public void onActivate() {
         spawnerPositions.clear();
+        deactivatedSpawnerPositions.clear();
     }
     @Override
     public void onDeactivate() {
         spawnerPositions.clear();
+        deactivatedSpawnerPositions.clear();
     }
     @EventHandler
     private void onPreTick(TickEvent.Pre event) {
@@ -122,12 +155,28 @@ public class ActivatedSpawnerDetector extends Module {
                     if (blockEntity instanceof MobSpawnerBlockEntity){
                         MobSpawnerBlockEntity spawner = (MobSpawnerBlockEntity) blockEntity;
                         BlockPos pos = spawner.getPos();
-                        if (!spawnerPositions.contains(pos) && spawner.getLogic().spawnDelay != 20) {
+                        if (!deactivatedSpawnerPositions.contains(pos) && !spawnerPositions.contains(pos) && spawner.getLogic().spawnDelay != 20) {
                             if (mc.world.getRegistryKey() == World.NETHER && spawner.getLogic().spawnDelay == 0) return;
-                            if (displaycoords.get()) ChatUtils.sendMsg(Text.of("Detected Triggered Spawner! Block Position: " + pos));
-                            else ChatUtils.sendMsg(Text.of("Detected Triggered Spawner!"));
+                            if (displaycoords.get()) ChatUtils.sendMsg(Text.of("Detected Activated Spawner! Block Position: " + pos));
+                            else ChatUtils.sendMsg(Text.of("Detected Activated Spawner!"));
                             if (extramessage.get()) error("There may be stashed items in the storage near the spawners!");
                             spawnerPositions.add(pos);
+                            if (deactivatedSpawner.get()){
+                                boolean lightsFound = false;
+                                for (int x = -deactivatedSpawnerdistance.get(); x < deactivatedSpawnerdistance.get()+1; x++) {
+                                    for (int y = -deactivatedSpawnerdistance.get(); y < deactivatedSpawnerdistance.get()+1; y++) {
+                                        for (int z = -deactivatedSpawnerdistance.get(); z < deactivatedSpawnerdistance.get()+1; z++) {
+                                            BlockPos bpos = new BlockPos(pos.getX()+x,pos.getY()+y,pos.getZ()+z);
+                                            if (mc.world.getBlockState(bpos).getBlock() == Blocks.TORCH || mc.world.getBlockState(bpos).getBlock() == Blocks.SOUL_TORCH || mc.world.getBlockState(bpos).getBlock() == Blocks.REDSTONE_TORCH || mc.world.getBlockState(bpos).getBlock() == Blocks.JACK_O_LANTERN || mc.world.getBlockState(bpos).getBlock() == Blocks.GLOWSTONE || mc.world.getBlockState(bpos).getBlock() == Blocks.SHROOMLIGHT || mc.world.getBlockState(bpos).getBlock() == Blocks.OCHRE_FROGLIGHT || mc.world.getBlockState(bpos).getBlock() == Blocks.PEARLESCENT_FROGLIGHT || mc.world.getBlockState(bpos).getBlock() == Blocks.PEARLESCENT_FROGLIGHT || mc.world.getBlockState(bpos).getBlock() == Blocks.SEA_LANTERN || mc.world.getBlockState(bpos).getBlock() == Blocks.LANTERN || mc.world.getBlockState(bpos).getBlock() == Blocks.SOUL_LANTERN || mc.world.getBlockState(bpos).getBlock() == Blocks.CAMPFIRE || mc.world.getBlockState(bpos).getBlock() == Blocks.SOUL_CAMPFIRE){
+                                                lightsFound = true;
+                                                deactivatedSpawnerPositions.add(pos);
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                                if (lightsFound == true) ChatUtils.sendMsg(Text.of("The Spawner has torches or other light blocks!"));
+                            }
                         }
                     }
                 }
@@ -148,7 +197,8 @@ public class ActivatedSpawnerDetector extends Module {
                         int endY = pos.getY();
                         int endZ = pos.getZ();
                         if (rangerendering.get())render(new Box(new Vec3d(startX+15, startY+15, startZ+15), new Vec3d(endX-14, endY-14, endZ-14)), rangeSideColor.get(), rangeLineColor.get(), shapeMode.get(), event);
-                        render(new Box(new Vec3d(startX+1, startY+1, startZ+1), new Vec3d(endX, endY, endZ)), spawnerSideColor.get(), spawnerLineColor.get(), shapeMode.get(), event);
+                        if (deactivatedSpawnerPositions.contains(pos)) render(new Box(new Vec3d(startX+1, startY+1, startZ+1), new Vec3d(endX, endY, endZ)), despawnerSideColor.get(), despawnerLineColor.get(), shapeMode.get(), event);
+                        else render(new Box(new Vec3d(startX+1, startY+1, startZ+1), new Vec3d(endX, endY, endZ)), spawnerSideColor.get(), spawnerLineColor.get(), shapeMode.get(), event);
                     }
                 }
             }
@@ -163,6 +213,7 @@ public class ActivatedSpawnerDetector extends Module {
         double renderDistanceBlocks = renderDistance.get() * 16;
 
         removeChunksOutsideRenderDistance(spawnerPositions, cameraPos, renderDistanceBlocks);
+        removeChunksOutsideRenderDistance(deactivatedSpawnerPositions, cameraPos, renderDistanceBlocks);
     }
     private void removeChunksOutsideRenderDistance(Set<BlockPos> chunkSet, BlockPos cameraPos, double renderDistanceBlocks) {
         chunkSet.removeIf(chunkPos -> !cameraPos.isWithinDistance(chunkPos, renderDistanceBlocks));
