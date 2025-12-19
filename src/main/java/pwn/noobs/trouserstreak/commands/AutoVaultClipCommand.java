fe8c79eea3b6bd1624391dda538c605e32d43b06
aisiaiiad <234670636+aisiaiiad@users.noreply.github.com>
2025-12-19 16:39:47 +0200

    Fix fall damage when clipping down.

--- src/main/java/pwn/noobs/trouserstreak/commands/AutoVaultClipCommand.java
+++ src/main/java/pwn/noobs/trouserstreak/commands/AutoVaultClipCommand.java
@@ -55,7 +55,8 @@ public class AutoVaultClipCommand extends Command {
                 BlockPos isopenair1 = (player.getBlockPos().add(0,i,0));
                 BlockPos isopenair2 = (player.getBlockPos().add(0,i-1,0));
                 if (mc.world.getBlockState(isopenair1).isReplaceable() && mc.world.getFluidState(isopenair1).isEmpty() && !mc.world.getBlockState(isopenair1).isOf(Blocks.POWDER_SNOW) && mc.world.getBlockState(isopenair2).isReplaceable() && mc.world.getFluidState(isopenair2).isEmpty() && !mc.world.getBlockState(isopenair2).isOf(Blocks.POWDER_SNOW)){
-                    int packetsRequired = 20;
+                    double blocks = isopenair2.getY() - mc.player.getY();
+                    int packetsRequired = (int) Math.ceil(Math.abs(blocks / 10));
                     if (player.hasVehicle()) {
                         Entity vehicle = player.getVehicle();
                         for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
@@ -66,7 +67,11 @@ public class AutoVaultClipCommand extends Command {
                     for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
                         mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true, mc.player.horizontalCollision));
                     }
-                    player.setPosition(player.getX(), isopenair2.getY(), player.getZ());
+                    mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), isopenair2.getY(), mc.player.getZ(), false, mc.player.horizontalCollision));
+                    mc.player.setPosition(mc.player.getX(), isopenair2.getY(), mc.player.getZ());
+                    double y = isopenair2.getY() + 0.0000000001;
+                    mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), y, mc.player.getZ(), false, mc.player.horizontalCollision)); // we are slightly higher, resets fall distance to 0
+                    mc.player.setPosition(mc.player.getX(), y, mc.player.getZ());
                     return SINGLE_SUCCESS;
                 }
             }
@@ -101,4 +106,5 @@ public class AutoVaultClipCommand extends Command {
             return SINGLE_SUCCESS;
         }));
     }
+
 }
