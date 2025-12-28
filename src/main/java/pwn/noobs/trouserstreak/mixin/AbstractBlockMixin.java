55bf0dd80f54ed7af0ad84231fa96cc3663c97d9
etianl <115842502+etianl@users.noreply.github.com>
2025-12-28 03:27:02 -0800

    1.5.5 added BoatNoclip, Mace and Spear Kill updates too!
    **v1.5.5**
    ## New BoatNoclip Module!
    Merged this pull request from [aisiaiiad](https://github.com/aisiaiiad) https://github.com/etianl/Trouser-Streak/pull/179
    This new module allows you to disable clipping of blocks when you are in a boat. Currently only tested to be working in Paper servers of Minecraft version 1.21.11. It likely works in more servers too! Does not work in vanilla server.
    
    ## More SpearKill updates!
    - Added a "Lunge Delay Modifier" option to **Lunge mode** of SpearKill as requested here: https://github.com/etianl/Trouser-Streak/issues/176
    
    Implemented new commits for **Blink mode** by [Kimtaeho](https://github.com/needitem) from the https://github.com/needitem/needitem-tweak/ repo!
    
    Included Changes:
    Improve Blink mode with target-based flush timing
    - Removed min/max delay ticks and min distance settings
    - Added Flush Range setting (distance to target when flush occurs)
    - Flush now triggers based on:
      1. Close to target (within flush range)
      2. Passed through target (approaching → moving away)
    - Auto-rotates towards target before flush for max damage
    - Better handling of moving/airborne targets
    
    Target Filtering:
    - New setting group for entity filtering
    - Whitelist/Blacklist mode with entity type selection
    
    Added 1-tick velocity flush for massive spear damage
    - Send start position then end position in same tick = huge velocity
    
    I also added a "Force Flush Distance" setting which forces a packet flush after however many blocks.
    
    
    Note: Lunge mode and timer do not work together!
    
    ## MaceKill update!
    Totem Bypass options added. 
    - **Randomize height:** Randomizes the fall height below the set limit.
    - **Height deviation:** Max blocks to subtract from max height (e.g. 40)
    
    These two options will randomize the upward teleport packet position.
    **This can bypass a player's hurt cooldown making their totem useless!!!**
    
    Thank you to [aisiaiiad](https://github.com/aisiaiiad) for the suggestion!
    Also special thanks to CookieMunscher for taking some time out of their evening to be my punching bag for testing!
    
    This is easiest to execute with something like the KillAura module that lets you attack every tick.
    
    ## Other things:
    - Made "Prevent Fall damage" option in **MaceKill** move you up an imperceivable amount instead of actually bumping you up a bit.
    - Made slot switching options invisible if "Shield Breaker" and "Shield Breaker No Swap" are both enabled in AttributeSwap

---
+++ src/main/java/pwn/noobs/trouserstreak/mixin/AbstractBlockMixin.java
@@ -0,0 +1,36 @@
+package pwn.noobs.trouserstreak.mixin;
+
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.EntityShapeContext;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import pwn.noobs.trouserstreak.modules.BoatNoclip;
+
+@Mixin(AbstractBlock.class)
+public class AbstractBlockMixin {
+    @Inject(method = "getCollisionShape", at = @At("HEAD"), cancellable = true)
+    private void getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
+        if (!(context instanceof EntityShapeContext esc)) return;
+        if (esc.getEntity() == null) return;
+
+        Entity entity = esc.getEntity();
+        if (Modules.get() == null) return;
+
+        if (Modules.get().isActive(BoatNoclip.class)
+            && entity instanceof BoatEntity boat
+            && boat.isControlledByPlayer()) {
+            cir.setReturnValue(VoxelShapes.empty());
+        }
+    }
+}
