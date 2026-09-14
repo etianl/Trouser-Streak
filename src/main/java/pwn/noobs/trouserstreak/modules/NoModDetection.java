eb1b65dd39fb6dc9fc4add4b023495ca4e3d88bb
etianl <115842502+etianl@users.noreply.github.com>
2026-09-13 22:12:49 -0700

    1.6.4 BeeCoordExploit added, CubePrimer crashfix and more
    #New Module
    ## Added BeeCoordExploit
    Scans a bee hive/nest in your main hand to tell you the locations of flowers that the bees within it last interacted with.
    
    **If the bee hive/nest came from a player's base this will expose the coordinates of their base.** 
    
    Example usage: 
    Ask players on a server if anyone has a bee hive/nest that you really need for a honey farm. You may offer them many diamonds for it.
    A player then grabs a bee hive/nest that happens to have bees in it already from their base.
    You then meet up with that player (maybe by /tpa) and obtain the bee hive/nest, which may provide you their base coordinates.
    
    
    # Bug Fixes and New Features:
    ## CubePrimer
    - Fixed potential "Components not bound yet" crash on startup. 
    ## NoModDetection
    Added settings for NoModDetection so you can customize which translation keys will use the fallback.
    The new "Filter Mode" setting allows you to choose which translation keys should be replaced.
    - PrefixList mode: The original hardcoded list of translation keys that I was using is the default here. You can add or remove whatever you want.
    - InstalledMods mode: Filter keys that are not vanilla translation keys. This means all your mods will use fallback keys. **This may break the text in some mods a little bit**
    - AllKeys mode: Filter every key in the game indiscriminately.
    
    **InstalledMods mode is now the default mode.**
    
    ## RemoteEnderChest
    - Ensure that we avoid processing slots that are not the player's inventory when using the "Item Saver".

--- src/main/java/pwn/noobs/trouserstreak/modules/NoModDetection.java
+++ src/main/java/pwn/noobs/trouserstreak/modules/NoModDetection.java
@@ -1,9 +1,63 @@
 package pwn.noobs.trouserstreak.modules;
 
+import meteordevelopment.meteorclient.settings.EnumSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.StringListSetting;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import pwn.noobs.trouserstreak.Trouser;
 
+import java.util.List;
+
 public class NoModDetection extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    public enum Modes {
+        InstalledMods,
+        PrefixList,
+        AllKeys
+    }
+    public final Setting<Modes> filterMode = sgGeneral.add(new EnumSetting.Builder<Modes>()
+            .name("filter-mode")
+            .description("PrefixList: List of translation keys. Add or remove whatever you want.\n" +
+                    "InstalledMods: Filter keys that are not vanilla translation keys **This may break the text in some mods a little bit**\n" +
+                    "AllKeys: Filter every key in the game indiscriminately.")
+            .defaultValue(Modes.InstalledMods)
+            .build());
+    // Most of these keys were gathered from https://github.com/branduzzo/CheckHacks/blob/main/src/main/resources/checkhacks.yml
+    // Thank you to the CheckHacks plugin for providing the things needed to prevent their own plugin from working
+    public final Setting<List<String>> keys = sgGeneral.add(new StringListSetting.Builder()
+            .name("key-prefixes")
+            .description("Translation key prefixes to spoof. If the beginning of the key matches this string use the fallback.")
+            .defaultValue(
+                    "key.meteor-client.",
+                    "module.",
+                    "addon.",
+                    "liquidbounce.module",
+                    "key.freecam",
+                    "key.wurst",
+                    "xray.config",
+                    "key.chestesp",
+                    "key.killaura",
+                    "key.autofish",
+                    "key.lumina",
+                    "bleachhack.module",
+                    "emc.module",
+                    "coffee.module",
+                    "key.wdl",
+                    "autoclicker-fabric.",
+                    "key.antiafk",
+                    "key.auto-clicker_",
+                    "key.ui-utils",
+                    "cracker.",
+                    "swd.",
+                    "litematica.",
+                    "gui.xaero",
+                    "baritone.",
+                    "voicechat."
+            )
+            .visible(() -> filterMode.get() == Modes.PrefixList)
+            .build()
+    );
     public NoModDetection() {
         super(Trouser.Main, "NoModDetection", "Modifies some translation keys to use a fallback instead. This may allow you to play on servers that do not allow mods.");
     }
